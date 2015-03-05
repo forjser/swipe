a12533ef066fb88d08c6364c875d64276998f256
codemark <thebeautyoflove@163.com>
2015-03-05 10:25:06 +0800

    init

---
+++ swipe.js
@@ -0,0 +1,37 @@
+	
+	function Swipe(param,param1){
+		this.leftCallback = param;
+		this.rightCallback = param1;
+	}
+	Swipe.prototype.temp = {
+		"startPageX" : '',
+		"startPageY" : ''
+	}
+	Swipe.prototype.start = function(){
+		var e = arguments[0];
+		e.preventDefault();
+		this.temp.startPageX = e.changedTouches[0].pageX;
+		this.temp.startPageY = e.changedTouches[0].pageY;
+	}
+	Swipe.prototype.end = function(){
+		var e = arguments[0];
+		e.preventDefault();
+		if(Math.abs(e.changedTouches[0].pageY - this.temp.startPageY) > 50 || Math.abs(e.changedTouches[0].pageX - this.temp.startPageX) < 20){
+			return;
+		}
+		if(e.changedTouches[0].pageX - this.temp.startPageX > 0){
+			this.swipeRight();//ÏòÓÒ
+		}else{
+			this.swipeLeft();//Ïò×ó
+		}
+	}
+	Swipe.prototype.swipeLeft = function(){
+		if(this.leftCallback)this.leftCallback();
+	}
+	Swipe.prototype.swipeRight = function(){
+		if(this.rightCallback)this.rightCallback();
+	}
+	Swipe.prototype.swipe = function($ele){
+		$ele.addEventListener("touchstart",a.start.bind(a),false);
+		$ele.addEventListener("touchend",a.end.bind(a),false);
+	}

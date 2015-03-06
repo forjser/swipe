63ddde841c70599ffc40000d8509ca50382ddc1b
codemark <thebeautyoflove@163.com>
2015-03-06 16:57:06 +0800

    封�

---
+++ js/lswipe.js
@@ -0,0 +1,64 @@
+﻿		var liu = (function(){
+			var L={};
+			var Y;
+			var event = "click swipeLeft".split(" ");
+			L.fn = {
+				log : function(){console.log('test_l');},
+				error : function(){console.log('error');},
+				on : function(eventname,callback){
+					this.leftCallback = callback.leftCallback ? callback.leftCallback : null;
+					this.rightCallback = callback.rightCallback ? callback.rightCallback : null;
+					for(var i=0;i<this.length;i++){
+						this.addEvent(this,this[i],eventname,callback,false);
+					}
+				},
+				addEvent : function(self,context,eventname,callback,boolen){
+					switch(eventname){
+						case "swipe" : context.addEventListener('touchstart',this.tstart.bind(self),boolen);
+										context.addEventListener('touchend',this.tend.bind(self),boolen);
+										break;
+						default : self.error();
+					}
+				},
+				tstart : function(){
+					var e = arguments[0];
+					e.preventDefault();
+					this.temp.startPageX = e.changedTouches[0].pageX;
+					this.temp.startPageY = e.changedTouches[0].pageY;
+				},
+				tend : function(){
+					var e = arguments[0];
+					e.preventDefault();
+					if(Math.abs(e.changedTouches[0].pageY - this.temp.startPageY) > 50 || Math.abs(e.changedTouches[0].pageX - this.temp.startPageX) < 20){
+						return;
+					}
+					if(e.changedTouches[0].pageX - this.temp.startPageX > 0){
+						console.log('向右');
+						this.rightCallback();//向右
+					}else{
+						console.log('向左');
+						this.leftCallback();//向左
+					}
+				}
+			}
+			L.fn.temp = {
+				"startPageX" : '',
+				"startPageY" : '',
+				"leftCallback" : '',
+				"rightCallback" : ''
+			}
+			L.Z = function(dom, selector) {
+				dom = [document.getElementById(dom)];
+				dom.__proto__ = L.fn;
+				return dom;
+			}
+			L.init = function(selector, context){
+				return L.Z(selector, context)
+			}
+			Y = function(selector, context){
+				return L.init(selector, context);
+			}
+			return Y;
+		})();
+		window.liu = liu;
+		window.Y === undefined && (window.Y = liu);
--- js/swipe.js
+++ js/swipe.js
@@ -3,10 +3,10 @@
 		if(!arguments.length) return;
 		this.leftCallback = callback.leftCallback ? callback.leftCallback : null;
 		this.rightCallback = callback.rightCallback ? callback.rightCallback : null;
-	}
-	Swipe.prototype.temp = {
-		"startPageX" : '',
-		"startPageY" : ''
+		this.temp = {
+			"startPageX" : '',
+			"startPageY" : ''
+		}
 	}
 	Swipe.prototype.start = function(){
 		var e = arguments[0];
@@ -21,18 +21,14 @@
 			return;
 		}
 		if(e.changedTouches[0].pageX - this.temp.startPageX > 0){
-			this.swipeRight();//向右
+			this.rightCallback();//向右
 		}else{
-			this.swipeLeft();//向左
+			this.leftCallback();//向左
 		}
-	}
-	Swipe.prototype.swipeLeft = function(){
-		if(this.leftCallback)this.leftCallback();
-	}
-	Swipe.prototype.swipeRight = function(){
-		if(this.rightCallback)this.rightCallback();
 	}
 	Swipe.prototype.swipe = function($ele,self){
 		$ele.addEventListener("touchstart",self.start.bind(self),false);
 		$ele.addEventListener("touchend",self.end.bind(self),false);
 	}
+	
+	
